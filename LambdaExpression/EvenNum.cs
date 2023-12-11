c0985eae3a7067404f16e571a62d727de57aa9db
sachin2k01 <thenamesachin@gmail.com>
2023-12-11 14:54:51 +0530

    add some lambda expressionss

---
+++ LambdaExpression/EvenNum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    public class EvenNum
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 3, 2, 5, 7, 98, 23, 87, 23,4 };
+
+            Func<int[], int> even = (int[] arr) =>
+            {
+                int count = 0;
+                foreach (int i in arr)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine($"Even number is: {i}");
+                        count++;
+                    }
+                    
+                }
+                return count;
+            };
+            Console.WriteLine("Total Number of even Numbers in array: "+ even(nums));            
+        }
+    }
+}
---
+++ LambdaExpression/EvenNumbers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpression
+{
+    class EvenNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 2, 3, 5, 4, 7, 9, 11, 45, 23, 87 };
+
+            Func<int[], int> even = (int[] arr) =>
+            {
+                foreach (int i in arr)
+                {
+                    if (i % 2 == 0)
+                        return i;
+                }
+            }
+
+        even(even);
+
+            foreach (int j in even)
+            {
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
